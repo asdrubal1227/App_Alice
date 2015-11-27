def18ae56b8724905e098d6f65603912f925f2a8
BrayanAsdrubal <brayan_ahg@hotmail.com>
2015-11-27 02:28:15 -0500

    Hola

--- app/src/main/java/com/example/brayanasdrubal/appalice/cuenta.java
+++ app/src/main/java/com/example/brayanasdrubal/appalice/cuenta.java
@@ -1,8 +1,11 @@
 package com.example.brayanasdrubal.appalice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 public class cuenta extends AppCompatActivity {
 
@@ -11,5 +14,15 @@ public class cuenta extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_cuenta);
+    }
+
+
+    public void onClick (View v){
+        Toast.makeText(cuenta.this, "Cuidador", Toast.LENGTH_SHORT).show();
+        //Abrir la actividad
+        Intent i = new Intent(cuenta.this, info_cuidador.class);
+        startActivity(i);
+        finish();
+
     }
 }
